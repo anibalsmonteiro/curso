e88d2d0399fa13fe2e21db7a05305bbd7b4fe807
Anibal Monteiro <anibalsmonteiro@gmail.com>
2020-02-28 14:03:18 -0300

    Shape abstract class

---
+++ Source/Entities/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using Source.Entities.Enums;
+
+namespace Source.Entities
+{
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle (double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+
+    }
+}
